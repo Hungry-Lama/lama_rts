3c96d553604e4d4962fd4900cd783445b44b5366
linzell <linzellart@gmail.com>
2022-11-03 21:54:14 +0100

    🔥 Add setup intial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use  bevy::{window::close_on_esc, prelude::*};
+
+fn main () {
+    App::new()
+      .insert_resource(Msaa { samples: 4 })
+      .insert_resource(WindowDescriptor {
+          title: "Bevy Game".to_string(),
+          width: 1280.,
+          height: 720.,
+          ..Default::default()
+      })
+      .add_startup_system(setup)
+      .add_system(close_on_esc)
+      .add_plugins(DefaultPlugins)
+    .run();
+}
+
+fn setup(
+    mut commands: Commands,
+) {
+  commands
+    .spawn_bundle(Camera3dBundle::default())
+    .commands();
+    // .spawn_bundle(UiCameraBundle::default());
 }
