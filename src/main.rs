d49f3ee7e53f811c9554fb24ae205454714bfae3
IshariFluttershy <tanguy.morvant2@gmail.com>
2022-11-08 02:12:43 +0100

    Added picking single character when left click

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use bevy::{window::close_on_esc, prelude::*, render::camera};
+use bevy::{window::close_on_esc, prelude::*, render::camera::{self, RenderTarget}};
+use bevy_mod_picking::*;
+
 // Exemple to I18n import
 use rust_i18n::t;
 
@@ -7,6 +9,8 @@ rust_i18n::i18n!("locales");
 
 fn main () {
   App::new()
+  .add_plugins(DefaultPlugins)
+  .add_plugins(DefaultPickingPlugins)
   .insert_resource(Msaa { samples: 4 })
   .insert_resource(WindowDescriptor {
     title: "Bevy Game".to_string(),
@@ -20,10 +24,11 @@ fn main () {
   .add_system_set(
     SystemSet::new()
     .with_system(move_camera)
-    .with_system(change_camera_data),
+    .with_system(change_camera_data)
+    .with_system(move_character),
   )
   .add_system(close_on_esc)
-  .add_plugins(DefaultPlugins)
+  .add_system_to_stage(CoreStage::PostUpdate, select_character_picking_event)
   .run();
 }
 
@@ -31,6 +36,12 @@ fn main () {
 struct CameraData {
     speed: f32,
 }
+
+#[derive(Component)]
+struct MainCamera;
+
+#[derive(Component)]
+struct Selected;
 
 fn setup(
     mut commands: Commands,
@@ -45,6 +56,8 @@ fn setup(
       transform: Transform::from_xyz(0.0, 15.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     })
+    .insert(MainCamera)
+    .insert_bundle(PickingCameraBundle::default())
     .commands();
     // .spawn_bundle(UiCameraBundle::default());
 }
@@ -65,19 +78,19 @@ fn spawn_basic_scene(
       material: materials.add(Color::rgb(0.67, 0.84, 0.92).into()),
       transform: Transform::from_xyz(0.0, 0.5, 0.0),
       ..default()
-  });
+  }).insert_bundle(PickableBundle::default());
   commands.spawn_bundle(PbrBundle {
     mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
     material: materials.add(Color::rgb(0.67, 0.84, 0.92).into()),
     transform: Transform::from_xyz(2.0, 0.5, 2.0),
     ..default()
-  });
+  }).insert_bundle(PickableBundle::default());
   commands.spawn_bundle(PbrBundle {
     mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
     material: materials.add(Color::rgb(0.67, 0.84, 0.92).into()),
     transform: Transform::from_xyz(-1.0, 0.5, 1.0),
     ..default()
-  });
+  }).insert_bundle(PickableBundle::default());
 }
 
 fn move_camera(
@@ -133,5 +146,37 @@ fn change_camera_data(
     camera_data.speed = 40.0;
   } else if camera_data.speed < 5.0 {
     camera_data.speed = 5.0;
+  }
+}
+
+fn move_character(
+  mut selectables: Query<&mut Transform, With<Selected>>
+) {
+  for mut transform in selectables.iter_mut() {
+    transform.translation += Vec3::X * 0.01;
+  }
+}
+
+pub fn select_character_picking_event(
+  mut commands: Commands,
+  mut events: EventReader<PickingEvent>
+) {
+  for event in events.iter() {
+      match event {
+          PickingEvent::Selection(e) => {
+            //info!("A selection event happened: {:?}", e);
+
+            match e {
+              SelectionEvent::JustDeselected(s) => {
+                commands.entity(*s).remove::<Selected>();
+              },
+              SelectionEvent::JustSelected(s) => {
+                commands.entity(*s).insert(Selected);
+              }
+            }
+          },
+          PickingEvent::Hover(e) => {/*info!("Egads! A hover event!? {:?}", e)*/},
+          PickingEvent::Clicked(e) => {/*info!("Gee Willikers, it's a click! {:?}", e)*/},
+      }
   }
 }
